55bbbc0d48c4a34c928e35fee2bd0497a16b26b8
cchanmi <113524@naver.com>
2024-07-18 22:28:30 +0900

    [Setting] 프로젝트 생성

---
+++ dnd-11th-4-iOS/dnd-11th-4-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dnd-11th-4-iOS
+//
+//  Created by 황찬미 on 7/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
