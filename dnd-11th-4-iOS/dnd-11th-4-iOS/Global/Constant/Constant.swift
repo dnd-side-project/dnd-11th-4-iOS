c404d0a4941611966992e6d0369b8b804cf07bf3
cchanmi <113524@naver.com>
2024-07-23 18:10:06 +0900

    [SETTING] #1 - 해상도 width, height 전역 변수 추가

---
+++ dnd-11th-4-iOS/dnd-11th-4-iOS/Global/Constant/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  dnd-11th-4-iOS
+//
+//  Created by 황찬미 on 7/23/24.
+//
+
+import UIKit
+
+struct Constant { }
+
+extension Constant {
+    struct Screen {
+        static let width = UIScreen.main.bounds.width
+        static let height = UIScreen.main.bounds.width
+    }
+}
