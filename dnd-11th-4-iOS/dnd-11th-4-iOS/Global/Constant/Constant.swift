d73e14cb942b14018d795f2c195f9b295ff1ae06
wooyani77 <wooyani77@gmail.com>
2024-07-31 22:20:53 +0900

    [ADD] #3 - Image Constant 추가

--- dnd-11th-4-iOS/dnd-11th-4-iOS/Global/Constant/Constant.swift
+++ dnd-11th-4-iOS/dnd-11th-4-iOS/Global/Constant/Constant.swift
@@ -14,4 +14,24 @@ extension Constant {
         static let width = UIScreen.main.bounds.width
         static let height = UIScreen.main.bounds.height
     }
+    
+    enum Image {
+        static let iconCalendar = UIImage(named: "iconCalendar")
+        static let iconBack = UIImage(named: "iconBack")
+        static let iconDetail = UIImage(named: "iconDetail")
+        static let iconForward = UIImage(named: "iconForward")
+        static let iconMap = UIImage(named: "iconMap")
+        static let iconMapBlack = UIImage(named: "iconMapBlack")
+        static let iconMapGray = UIImage(named: "iconMapGray")
+        static let iconMyPage = UIImage(named: "iconMyPage")
+        static let iconMyPageBlack = UIImage(named: "iconMyPageBlack")
+        static let iconMyPageGray = UIImage(named: "iconMyPageGray")
+        static let iconPencilBlack = UIImage(named: "iconPencilBlack")
+        static let iconPencilGray = UIImage(named: "iconPencilGray")
+        static let iconPicture = UIImage(named: "iconPicture")
+        static let iconSetting = UIImage(named: "iconSetting")
+        static let iconShoes = UIImage(named: "iconShoes")
+        static let iconTrashcan = UIImage(named: "iconTrashcan")
+        static let iconXCircle = UIImage(named: "iconXCircle")
+    }
 }
